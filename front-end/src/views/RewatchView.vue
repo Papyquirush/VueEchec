e8421af5574b5636c350d938cb2bbcd6003c1856
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-01-05 22:08:19 +0100

    rewatch

---
+++ front-end/src/views/RewatchView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-blue-950"><HeaderVue/></div>
+  <ChessBoard />
+</template>
+
+<style>
+</style>
+
+<script setup lang="ts">
+import HeaderVue from '@/components/HeaderVue.vue'
+import ChessBoard from '@/components/ChessBoardRewatch.vue'
+</script>
