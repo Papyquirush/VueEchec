fb264ed7ad67fc7d129009fed029b41048bec169
p2200062 <mateo.rodriguez@etu.univ-lyon1.fr>
2025-01-05 19:26:49 +0100

    correction erreur

--- backend/src/services/game.services.ts
+++ backend/src/services/game.services.ts
@@ -67,7 +67,7 @@ export class GameService {
         }
     }
 
-    public async nextTurn(id: number, oldPosition: string, position: string,resetRuleFiftyMoves:boolean): Promise<void> {
+    public async nextTurn(id: number, oldPosition: string, position: string,resetRuleFiftyMoves:boolean) {
         let game = await Game.findByPk(id);
         if (game) {
             let gameState = new GameState(game.id);
@@ -78,12 +78,12 @@ export class GameService {
             await gameState.updateGameState(oldPosition, position);
 
             if (gameState.pieces[position].color === 'white') {
-                let chessPiece = await ChessPiece.findOne({ where: { position: position, game_id: game.id } });
+                let chessPiece = await ChessPiece.findOne({ where: { position: oldPosition, game_id: game.id } });
                 if(chessPiece) {
                     await moveServices.createMove(game.id, game.turn_count, game.player_white_id, chessPiece.id, oldPosition, position, 0);
                 }
             } else {
-                let chessPiece = await ChessPiece.findOne({ where: { position: position, game_id: game.id } });
+                let chessPiece = await ChessPiece.findOne({ where: { position: oldPosition, game_id: game.id } });
                 if(chessPiece) {
                     await moveServices.createMove(game.id, game.turn_count, game.player_black_id, chessPiece.id, oldPosition, position, 0);
                 }
@@ -93,6 +93,10 @@ export class GameService {
                 await this.finishGame(id,undefined);
             }else{
                 await this.updateGame(id, game.player_white_id, game.player_black_id, game.is_public, gameState.pieces, game.is_finished, undefined, game.turn_count + 1,undefined,game.count_rule_fifty_moves);
+            }
+            console.log(id)
+            if(await this.isPat(id)){
+                await this.finishGame(id,undefined);
             }
         }
     }
@@ -141,7 +145,11 @@ export class GameService {
 
 
             await this.updateGame(id, game.player_white_id, game.player_black_id, game.is_public, gameState.pieces, game.is_finished, undefined, game.turn_count + 1,undefined,1);
+            if(await this.isPat(id)){
+                await this.finishGame(id,undefined);
+            }
         }
+        
     }
 
 
@@ -162,8 +170,11 @@ export class GameService {
             }else{
                 await this.updateGame(id, game.player_white_id, game.player_black_id, game.is_public, gameState.pieces, game.is_finished, undefined, game.turn_count + 1,undefined,game.count_rule_fifty_moves);
             }
+            if(await this.isPat(id)){
+                await this.finishGame(id,undefined);
+            }
         }
-
+       
     }
 
     public async getLastGame(userId: number): Promise<GameDTO | null> {
@@ -330,6 +341,7 @@ export class GameService {
 
     public async isPat(gameId: number) {
         let game = await this.getGameById(gameId);
+        console.log(game)
         let turnWhite = game.turnCount % 2 === 0;
         let pieces = [];
         for (let position in game.gameState) {
@@ -337,16 +349,20 @@ export class GameService {
                 let piece = new ChessPiece();
                 piece.position = position;
                 piece.color = game.gameState[position].color;
-                piece.piece_type = game.gameState[position].type;
+                piece.piece_type = game.gameState[position].pieceType;
+                piece.game_id = game.id;
                 pieces.push(piece);
             }
         }
         for (let piece of pieces) {
-            let slots = await chessPieceServices.getSlotsAvailable(piece.position, gameId);
-            if(slots.length > 0) {
+            
+            let chessPiece = chessPieceServices.convertToSpecificPiece(piece);
+            let slots = chessPiece.getSlotsAvailable(false,game);
+            if((await slots).length>0){
                 return false;
             }
         }
+        return true;
     }
 }
 
